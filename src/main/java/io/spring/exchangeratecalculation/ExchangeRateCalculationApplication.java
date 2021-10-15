eca3bd2c12466da2002a454962fbb78ca3a7d825
jaesungahn91 <jaesungahn91@gmail.com>
2021-10-16 00:11:49 +0900

    init

---
+++ src/main/java/io/spring/exchangeratecalculation/ExchangeRateCalculationApplication.java
@@ -0,0 +1,13 @@
+package io.spring.exchangeratecalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateCalculationApplication.class, args);
+    }
+
+}
